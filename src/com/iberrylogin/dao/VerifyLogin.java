c396e3c17e3290ac92c25204a66036d128e83ac8
Unitech17 <Unitech17@Unitech18-PC>
2017-07-07 10:53:15 +0530

    VerifyLogin program by B.Swetha

--- src/com/iberrylogin/dao/VerifyLogin.java
+++ src/com/iberrylogin/dao/VerifyLogin.java
@@ -8,10 +8,34 @@ import java.sql.SQLException;
 import java.sql.*;
 
 public class VerifyLogin {
-    public static boolean validate(String name, String pass) {
-        //This program validates the user id and password
+    public static boolean validate(String name, String pass) 
+    {
     	boolean status = false;
-        
-        return status;
+    	try
+    	{
+        //This program validates the user id and password
+    	Class.forName("com.mysql.jdbc.Driver");
+		Connection con=DriverManager.getConnection("jdbc:mysql://localhost:3306/akshitha","root","root");
+		PreparedStatement pst=con.prepareStatement("select *from user"); 
+		pst.executeQuery();
+		
+		ResultSet rs=pst.executeQuery();
+		while(rs.next())
+		{
+		if(	rs.getString(1).equals(name) && rs.getString(2).equals(pass))
+		{
+			status=true;//WELCOME
+			
+		}
     }
-}
+    	}
+	catch(Exception e)
+	{
+		e.printStackTrace();
+	}
+	return status;
+	
+	}	
+    	
+       
+    }
