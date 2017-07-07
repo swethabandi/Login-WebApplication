048069ed6490b335ecbbda73326e90a3ac88f9d0
Unitech17 <Unitech17@Unitech18-PC>
2017-07-07 10:54:18 +0530

    LoginServlet program byB.Swetha

--- src/com/iberrylogin/servlets/LoginServlet.java
+++ src/com/iberrylogin/servlets/LoginServlet.java
@@ -1,11 +1,16 @@
 package com.iberrylogin.servlets;
 
 import java.io.IOException;
+import java.io.PrintWriter;
+
+import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
+
+import com.iberrylogin.dao.VerifyLogin;
 
 /**
  * Servlet implementation class LoginServlet
@@ -25,10 +30,30 @@ public class LoginServlet extends HttpServlet {
 	/**
 	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
 	 */
-	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException
+	{
 		// TODO Auto-generated method stub
+		
 		response.getWriter().append("Served at: ").append(request.getContextPath());
+		PrintWriter out=response.getWriter();
+		response.setContentType("text/html");
+		String un=request.getParameter("Username");
+		String ps=request.getParameter("Password");
+	boolean x= VerifyLogin.validate(un ,ps);
+	if(x)
+	{
+		RequestDispatcher rd=request.getRequestDispatcher("welcome.jsp");
+		rd.forward(request,response);
 	}
+	else
+	{
+		RequestDispatcher rd=request.getRequestDispatcher("error.jsp");
+		rd.forward(request,response);
+	
+	}
+	}
+		
+	//response.getWriter().append("Served at: ").append(request.getContextPath());
 
 	/**
 	 * @see HttpServlet#doPost(HttpServletRequest request, HttpServletResponse response)
